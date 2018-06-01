4d9639236dcd597a382fb7410fba2496a6f1f05d
JakeLandowski <jakelandowski@gmail.com>
2018-06-01 12:43:15 -0700

    Got a semi working graph creator

---
+++ js/vertices.js
@@ -0,0 +1,106 @@
+//============ SETUP ===============//
+
+const two = new Two
+({
+    fullscreen: true,
+    type: Two.Types.canvas
+});
+
+const main = document.getElementById('main');
+two.appendTo(main);
+
+window.onresize = function()
+{
+    two.update();
+};
+
+const canvas = main.getElementsByTagName('canvas')[0];
+
+function rand(min, max) 
+{
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//============ RUN ===============//
+
+const edgeRenderingGroup = two.makeGroup();
+const edgeGroup   = two.makeGroup();
+const vertexGroup = two.makeGroup();
+
+canvas.onclick = function(event)
+{
+    let x = event.offsetX;
+    let y = event.offsetY;
+
+    let vertex = two.makeCircle(x, y, 20);
+    vertex.fill = "#9911ff";
+    vertex.linewidth = 4;
+    
+    let vertices = vertexGroup.children;
+
+    if(vertices.length > 1)
+    {
+        let randVertex = vertices[rand(0, vertices.length - 1)];
+        let edge = two.makeLine(x, y, x, y);
+
+        edge.stroke = "black";
+        edge.linewidth = 2;
+        edge.finalX = randVertex.translation.x;
+        edge.finalY = randVertex.translation.y;
+
+        edgeRenderingGroup.add(edge);
+    } 
+
+    vertexGroup.add(vertex);
+};
+
+
+// Run
+two.bind('update', function(frameCount)
+{
+    let newEdges = edgeRenderingGroup.children;
+    
+    if(newEdges.length > 0)
+    {
+        newEdges.map(function(edge)
+        {
+            let secondX = edge.vertices[1].x + edge.translation.x;
+            let secondY = edge.vertices[1].y + edge.translation.y; 
+
+            if(secondX > edge.finalX - 10 && secondX < edge.finalX + 10 && 
+               secondY > edge.finalY - 10 && secondY < edge.finalY + 10)
+            // if(secondX == edge.finalX && secondY == edge.finalY)
+            {
+                edgeRenderingGroup.remove(edge);
+                edgeGroup.add(edge);
+            }
+
+            // edge.vertices[1].x = edge.finalX;
+            // edge.vertices[1].y = edge.finalY;
+
+            // console.log("vert x : " + edge.vertices[1].x + " final x : " + edge.finalX);
+
+            // two.makeCircle(edge.finalX, edge.finalY, 20);
+
+            if(secondX < edge.finalX - 10)
+            {
+                edge.vertices[1].x += 3;
+            }
+            else if(secondX > edge.finalX + 10)
+            {
+                edge.vertices[1].x -= 3;
+            }
+
+            if(secondY < edge.finalY - 10)
+            {
+                edge.vertices[1].y += 3;
+            }
+            else if(secondY > edge.finalY + 10)
+            {
+                edge.vertices[1].y -= 3;
+            }   
+        });
+    }
+});
+
+two.play();
