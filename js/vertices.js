6ef114b47180fec933f51ae8720b854d9d201287
JakeLandowski <jakelandowski@gmail.com>
2018-06-02 18:31:27 -0700

    Cleaned up a little of the code and make the edge drawing a lot smoother

--- js/vertices.js
+++ js/vertices.js
@@ -21,22 +21,54 @@ function rand(min, max)
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function createEdge(fromX, fromY, currentVertices)
+{
+    for(let i = 0; i < rand(1, currentVertices.length - 1); i++)
+    {
+        let randVertex = currentVertices[rand(0, currentVertices.length - 1)];
+        let edge = two.makeLine(fromX, fromY, fromX, fromY);
+    
+        edge.stroke = "black";
+        edge.linewidth = edgeWidth;
+        
+        edge.finalX = randVertex.translation.x;
+        edge.finalY = randVertex.translation.y;
+
+        edgeRenderingGroup.add(edge);
+    }
+}
+
+function linePosition(line, start=true)
+{
+    return {
+        x: line.vertices[start ? 0 : 1].x + line.translation.x,
+        y: line.vertices[start ? 0 : 1].y + line.translation.y
+    };
+}
+
 //============ RUN ===============//
 
+const edgeSpeed = 4;
+const vertexSize = 10;
+const vertexOutlineSize = vertexSize / 4;
+const edgeWidth = vertexOutlineSize;
+
 const edgeRenderingGroup = two.makeGroup();
-const edgeGroup   = two.makeGroup();
+const edgeGroup = two.makeGroup();
+const edges = edgeGroup.children;
+
 const vertexGroup = two.makeGroup();
+const vertices = vertexGroup.children;
+
 
 canvas.onclick = function(event)
 {
     let x = event.offsetX;
     let y = event.offsetY;
 
-    let vertex = two.makeCircle(x, y, 20);
+    let vertex = two.makeCircle(x, y, vertexSize);
     vertex.fill = "#9911ff";
-    vertex.linewidth = 4;
-    
-    let vertices = vertexGroup.children;
+    vertex.linewidth = vertexOutlineSize;
 
     if(vertices.length > 1)
     {
@@ -46,67 +78,53 @@ canvas.onclick = function(event)
     vertexGroup.add(vertex);
 };
 
-function createEdge(fromX, fromY, currentVertices)
-{
-    for(let i = 0; i < rand(1, currentVertices.length - 1); i++)
-    {
-        let randVertex = currentVertices[rand(0, currentVertices.length - 1)];
-        let edge = two.makeLine(fromX, fromY, fromX, fromY);
-    
-        edge.stroke = "black";
-        edge.linewidth = 4;
-        edge.finalX = randVertex.translation.x;
-        edge.finalY = randVertex.translation.y;
-    
-        edgeRenderingGroup.add(edge);
-    }
-}
-
 
 // Run
 two.bind('update', function(frameCount)
 {
     let newEdges = edgeRenderingGroup.children;
-    
+
     if(newEdges.length > 0)
     {
         newEdges.map(function(edge)
         {
-            let secondX = edge.vertices[1].x + edge.translation.x;
-            let secondY = edge.vertices[1].y + edge.translation.y; 
+            let linePos  = linePosition(edge, false);  
+            let finalX   = edge.finalX;
+            let finalY   = edge.finalY;
 
-            if(secondX > edge.finalX - 10 && secondX < edge.finalX + 10 && 
-               secondY > edge.finalY - 10 && secondY < edge.finalY + 10)
-            // if(secondX == edge.finalX && secondY == edge.finalY)
+            if(linePos.x > finalX - vertexSize/2 && linePos.x < finalX + vertexSize/2 && 
+               linePos.y > finalY - vertexSize/2 && linePos.y < finalY + vertexSize/2)
             {
                 edgeRenderingGroup.remove(edge);
                 edgeGroup.add(edge);
+                delete edge.finalX;
+                delete edge.finalY;
             }
 
-            // edge.vertices[1].x = edge.finalX;
-            // edge.vertices[1].y = edge.finalY;
+            
+            let velX = (finalX - linePos.x) / 10;
+            let velY = (finalY - linePos.y) / 10;
+            
+            edge.vertices[1].x += velX;
+            edge.vertices[1].y += velY;
 
-            // console.log("vert x : " + edge.vertices[1].x + " final x : " + edge.finalX);
+            // if(linePos.x < finalX - edgeSpeed)
+            // {
+            //     edge.vertices[1].x += edgeSpeed;
+            // }
+            // else if(linePos.x > finalX + edgeSpeed)
+            // {
+            //     edge.vertices[1].x -= edgeSpeed;
+            // }
 
-            // two.makeCircle(edge.finalX, edge.finalY, 20);
-
-            if(secondX < edge.finalX - 10)
-            {
-                edge.vertices[1].x += 3;
-            }
-            else if(secondX > edge.finalX + 10)
-            {
-                edge.vertices[1].x -= 3;
-            }
-
-            if(secondY < edge.finalY - 10)
-            {
-                edge.vertices[1].y += 3;
-            }
-            else if(secondY > edge.finalY + 10)
-            {
-                edge.vertices[1].y -= 3;
-            }   
+            // if(linePos.y < finalY - edgeSpeed)
+            // {
+            //     edge.vertices[1].y += edgeSpeed;
+            // }
+            // else if(linePos.y > finalY + edgeSpeed)
+            // {
+            //     edge.vertices[1].y -= edgeSpeed;
+            // }   
         });
     }
 });
